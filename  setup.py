4e20c307c05e34ed961d29e2d4be99ff9f02f7a1
barna154 <barnax.craft@gmail.com>
2025-11-09 19:30:28 +0100

    Valtoztatas

---
+++  setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+from glob import glob
+import os
+
+package_name = 'way_finder'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), 
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='barna154',
+    maintainer_email='simonbarna154@gmail.com',
+    description='TODO: Package description',
+    license='GNU General Public License v3.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'megoldas.py = megoldas_sim24.simple_pursuit:main',
+            'joystick_teleop_node = megoldas_sim24.joystick_teleop:main',
+        ],
+    },
+)
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
-# Robot irányítás lidar segítségével
-ROS 2 C++ package.  [![Static Badge](https://img.shields.io/badge/ROS2-Jazzy-blue)](https://docs.ros.org/en/humble/)
+# Robot irányítás lidar segítségével iskolai projekhez
+ROS 2 C++ package.  [![Static Badge](https://img.shields.io/badge/ROS_2-Humble-34aec5)][![Static Badge](https://img.shields.io/badge/ROS2-Jazzy-blue)](https://docs.ros.org/en/humble/)
 ## Packages and build
+
