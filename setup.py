e4a6ba9ad5dbae26dcc72500fcb551df9e35d3dd
barna154 <barnax.craft@gmail.com>
2025-11-09 20:40:03 +0100

    k

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+            [package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name), glob('launch/*launch.[pxy][yma]*')), 
     ],
